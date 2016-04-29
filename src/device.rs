d28f29dd6a49b5031c92be0a5515ca9ef7a1dc5b
Qian Linfeng <thewawar@gmail.com>
2016-04-29 17:25:48 +0800

    Add Network/File device

--- src/device.rs
+++ src/device.rs
@@ -1,13 +1,64 @@
 
+use std::io;
+use std::fs;
+use std::net;
 
-pub struct Usb {
-}
 
-pub struct Serial {
-}
+pub struct Usb {}
+pub struct Serial {}
 
+#[derive(Debug)]
 pub struct Network {
+    _host: String,
+    _port: u16,
+    stream: net::TcpStream
 }
 
+impl Network {
+    pub fn new(host: &str, port: u16) -> Network {
+        let stream = net::TcpStream::connect((host, port)).unwrap();
+        Network{
+            _host: host.to_string(),
+            _port: port,
+            stream: stream
+        }
+    }
+}
+
+impl io::Write for Network {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.stream.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+
+#[derive(Debug)]
 pub struct File {
+    _path: String,
+    fobj: fs::File
+}
+
+impl File {
+    pub fn new(path: &str) -> File {
+        let fobj = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(path)
+            .unwrap();
+        File{_path: path.to_string(), fobj: fobj}
+    }
+}
+
+impl io::Write for File {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.fobj.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.fobj.flush()
+    }
 }
