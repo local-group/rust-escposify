a2d1fb71ba510290ea7fa405603580e4f83773d3
Qian Linfeng <thewawar@gmail.com>
2016-04-29 23:05:47 +0800

    Add a simple example

---
+++ examples/simple.rs
@@ -0,0 +1,23 @@
+extern crate escposify;
+extern crate tempfile;
+
+use escposify::printer::Printer;
+use escposify::device::File;
+
+use tempfile::{NamedTempFile};
+
+fn main() {
+    let mut printer = Printer::new(
+        File::<NamedTempFile>::from_temp(), None, None);
+    let _ = printer
+        .font("C")
+        .align("lt")
+        .style("bu")
+        .size(0, 0)
+        .text("The quick brown fox jumps over the lazy dog")
+        .text("敏捷的棕色狐狸跳过懒狗")
+        .barcode("12345678", "EAN8", "", "", 0, 0)
+        .feed(1)
+        .cut(false)
+        .flush();
+}
