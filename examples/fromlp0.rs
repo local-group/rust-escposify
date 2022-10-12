617670f49ad473a8926a52c04f828a59e31ebab4
hgrahamcs <henry@hgrahamcs.com>
2022-10-11 22:12:42 -0500

    added fromlp0.rs to examples

---
+++ examples/fromlp0.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io;
+
+use escposify::printer::Printer;
+
+fn main() -> io::Result<()> {
+    let device_file = File::options().append(true).open("/dev/usb/lp0").unwrap();
+
+    let file = escposify::device::File::from(device_file);
+    let mut printer = Printer::new(file, None, None);
+
+    printer
+        .chain_size(0,0)?
+        .chain_text("The quick brown fox jumps over the lazy dog")?
+        .chain_feed(1)?
+        .chain_cut(false)?
+        .flush()
+}
